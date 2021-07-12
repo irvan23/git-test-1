9fab87e1875d4ff6ff4c34ba023e8a432c0af301
irvan23 <irvansusman@gmail.com>
2021-07-13 01:25:08 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, quisquam, suscipit quis officiis natus tempora cum quasi magni nihil sequi soluta atque non magnam sint? Esse cum maiores sunt voluptas.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
